83fbbd46c7e016a93866aad38428e6fe940026eb
matt <mbw21490@gmail.com>
2013-11-21 09:49:13 -0500

    normal illustration

--- memMain.java
+++ memMain.java
@@ -5,7 +5,7 @@ public class memMain
 	
 	public static void main(String[] args) 
 	{
-	System.out.println("|\n"+"|\n"+"|\n"+"|\n"+"|\n"+"|\n"+"|\n"+"|\n"+"|"+"         "+"|\n"+"|\n"+"|\n"+"|\n"+"|\n"+"|\n"+"|\n"+"|\n"+"|");
+	System.out.println("|\n"+"|\n"+"|\n"+"|\n"+"|\n"+"|\n"+"|\n"+"|\n"+"|");
 	System.out.println("---------");
 	
 
